970c48a8c7693fb3d62ac493d9f6acbdb2f0e4d8
goldsilver12 <vladyslav.starichenko@nure.ua>
2022-12-17 13:21:21 +0100

    Liquibase data migration

--- src/main/java/com/nure/ua/Volunteering_UA/VolunteeringUaApplication.java
+++ src/main/java/com/nure/ua/Volunteering_UA/VolunteeringUaApplication.java
@@ -2,10 +2,33 @@ package com.nure.ua.Volunteering_UA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @SpringBootApplication
 public class VolunteeringUaApplication {
 
+
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.gmail.com");
+		mailSender.setPort(587);
+
+		mailSender.setUsername("goldsilver1234512345@gmail.com");
+		mailSender.setPassword("wxgwswwflypewxko");
+
+		Properties props = mailSender.getJavaMailProperties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.debug", "true");
+
+		return mailSender;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VolunteeringUaApplication.class, args);
 	}
